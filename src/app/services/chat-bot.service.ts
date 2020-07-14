fae5c61268ac0d70a30a374ee494523496641aa2
neerajCYG <neeraj211090@yahoo.com>
2020-07-14 14:10:06 +0530

    changes

--- src/app/services/chat-bot.service.ts
+++ src/app/services/chat-bot.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ChatBotService {
-  url= "https://heroku-node-bakaiteers.herokuapp.com/output?message="
+  url= "http://heroku-node-bakaiteers.herokuapp.com/output?message="
   constructor(private http:HttpClient) { }
 
   getMessageOutput(inputText:string){
